56ae6f2a004b08b659bf20e244ae1d0509ad1d2d
The Duc Pham <62134515+PhamThe-KHDL@users.noreply.github.com>
2022-03-09 17:17:41 +0700

    WEEK 03

---
+++ THỰC HÀNH/CLASS/WEEK 03/MapReduce data set - Slide 3/mapper.py
@@ -0,0 +1,9 @@
+import sys
+#Word Count Example
+# input comes from standard input STDIN
+for line in sys.stdin:
+    line = line.strip() #remove leading and trailing whitespaces
+    words = line.split() #split the line into words and returns as a list
+    for word in words:
+    #write the results to standard output STDOUT
+        print'%s    %s' % (word,1) #Emit the word
